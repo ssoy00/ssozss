489cf2604e722f90a71679dcfa7ec2a115a69e91
KimSoYoung <ggomag3243@gmail.com>
2024-04-17 17:48:37 +0900

    최종_최종_진짜최종

--- js/b_cat.js
+++ js/b_cat.js
@@ -1,14 +1,9 @@
 window.onload = function() {
-    const character = document.getElementById('last_b_cat');
-    const newImage = document.getElementById('memory');
+  const character = document.getElementById('last_b_cat');
 
-    // 페이지가 로드되면 2초 후에 캐릭터를 점점 사라지게 만듭니다.
-    setTimeout(() => {
-      character.style.opacity = '0';
-    }, 10000);
-
-    // 3초 후에 새로운 이미지를 점점 투명하게 만들어서 나타나게 합니다.
-    setTimeout(() => {
-      newImage.style.opacity = '1';
-    }, 1000);
+  if (window.innerWidth <= 1000) {
+      setTimeout(() => {
+          character.style.display = 'None';
+      }, 3000);
   }
+}
